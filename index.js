9249af727bbefdca3f2a82e60b2e7a1ac522f6c7
jmarualgit <jairelmarual@gmail.com>
2024-02-13 00:40:53 -0800

    Create function to play one round
    Brute forced the combinations. Made use of previously made function getComputerChoice and sent in a hard coded choice for playerSelection to determine the winner

--- index.js
+++ index.js
@@ -4,12 +4,59 @@ function getComputerChoice() {
 
     switch (randomNumber) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 // console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    // case for when there's a tie, can just exit out of the function instead of 
+    // running through all the other code
+    if (playerSelection == computerSelection) {
+        return 'It was a tie!';
+    }
+
+    let result = 'W';
+
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'scissor') {
+            result = 'W';   // rock v scissors  W
+        } else {
+            result = 'L';   // rock v paper     L
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            result = 'W';   // paper v rock     W
+        } else {
+            result = 'L';   // paper v scissor  L
+        }
+    } else {        // scissors
+        if (computerSelection == 'paper') {
+            result = 'W';   // scissor v paper  W
+        } else {
+            result = 'L';   // paper v rock     L
+        }
+    }
+
+    if (result == 'W') {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else {
+        return `You lose! ${playerSelection} loses to ${computerSelection}!`;
+    }
+}
+
+const playerSelection = 'Scissors';
+const computerSelection = getComputerChoice();
+
+let result = playRound(playerSelection, computerSelection);
+
+console.log(`Player chooses ${playerSelection}!`)
+console.log(`Computer chooses ${computerSelection}!`)
+console.log(result);
