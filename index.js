643650394bad8efb450a7846df093ce71b1a9e48
jmarualgit <jairelmarual@gmail.com>
2024-02-13 00:20:47 -0800

    Create function for computer opponent
    Implemented a basic function to choose and return between rock, paper, or scissors

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("test!")
+// randomly returns rock, paper, or scissors
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
+
+// console.log(getComputerChoice())
